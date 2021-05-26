43fc35b565b6ce85ebe8d7267200389e3e1b95d2
“dongyuekai” <457545648@qq.com>
2021-05-26 10:17:36 +0800

    配置解析JSON格式的表单请求体数据解析json数据

--- MongoDB/article-bed/app.js
+++ MongoDB/article-bed/app.js
@@ -1,12 +1,22 @@
 const express = require('express')
+const { MongoClient, ObjectID } = require('mongodb')
+
+const connectUri = 'mongodb://localhost:27017'
+const dbClient = new MongoClient(connectUri)
 
 const app = express()
+
+// 配置解析请求体数据 application/json
+// 它会把解析到的请求体数据放到req.body中
+// 在使用之前挂载这个中间件
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
 app.post('/articles', (req, res) => {
+  console.log('req.body---', req.body)
   res.send('返回值---- post /articles')
 })
 
