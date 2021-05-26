07fd1bd0b2c262bc8cbbf4c6bdad3a2702680af0
“dongyuekai” <457545648@qq.com>
2021-05-26 10:38:22 +0800

    初始化路由配置（创建文章、错误处理中间件）

--- MongoDB/article-bed/app.js
+++ MongoDB/article-bed/app.js
@@ -15,11 +15,50 @@ app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
-app.post('/articles', (req, res) => {
-  console.log('req.body---', req.body)
-  res.send('返回值---- post /articles')
+// 创建文章API
+app.post('/articles', async (req, res, next) => {
+  try {
+    // 1. 获取客户端表单数据
+    const { article } = req.body
+
+    // 2. 数据验证
+    if (!article || !article.title || !article.description || !article.body) {
+      return res.status(422).json({
+        error: '请求参数不符合规则要求'
+      })
+    }
+
+    // 3. 把验证通过的数据插入数据库中
+    //    成功 -> 发送成功响应
+    //    失败 -> 发送失败响应
+    await dbClient.connect()
+
+    const collection = dbClient.db('test').collection('articles')
+
+    article.createdAt = new Date()
+    article.updatedAt = new Date()
+    const ret = await collection.insertOne(article)
+
+    article._id = ret.insertedId
+
+    res.status(201).json({
+      article
+    })
+  } catch (err) {
+    // 由错误处理中间件统一处理
+    next(err)
+    // res.status(500).json({
+    //   error: err.message
+    // })
+  }
 })
 
+// 错误处理中间件
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    error: err.message
+  })
+})
 
 
 app.listen(3000, () => {
